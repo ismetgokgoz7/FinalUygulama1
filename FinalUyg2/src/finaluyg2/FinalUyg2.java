7532f2f0dd4113787f22d4d2a1f18de891a485a4
ismetgokgoz7 <66409323+ismetgokgoz7@users.noreply.github.com>
2020-06-04 23:03:05 +0300

    Add files via upload

---
+++ FinalUyg2/src/finaluyg2/FinalUyg2.java
@@ -0,0 +1,16 @@
+
+package finaluyg2;
+enum gunler {PAZARTESİ,SALI,ÇARSAMBA,PERŞEMBE,CUMA,CUMARTESİ,PAZAR};
+//gunler adında bir enum oluşturuldu.
+
+
+public class FinalUyg2 {
+
+    public static void main(String[] args) {
+         gunler birgun=gunler.PAZARTESİ;
+      System.out.println("Seçilen Gün:"+birgun);
+   } 
+       
+    }
+    
+
